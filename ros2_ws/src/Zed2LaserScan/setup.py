9802b6fc879d2aae391e481cac2c6f3d45b48d1c
Karanpreet Raja <86526687+KaranpreetRaja@users.noreply.github.com>
2023-07-05 04:57:35 -0400

    Created Zed2LaserScan Base Template

---
+++ ros2_ws/src/Zed2LaserScan/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'Zed2LaserScan'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='karan',
+    maintainer_email='86526687+KaranpreetRaja@users.noreply.github.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
